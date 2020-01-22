298db4c45a93e5b83fcf60bc1f36307058bfaea6
Marcelo <mgouveiadeassis@gmail.com>
2020-01-22 20:06:19 -0300

    add page template

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" 
+            id="github_username" 
+            required 
+            onChange={e => setGithubUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" 
+            id="techs" 
+            required 
+            onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="number"
+               name="latitude"
+               id="latitude" 
+               required 
+               value={latitude}
+               onChange={e => setLatitude(e.target.value)}
+               />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="number" 
+              name="longitude" 
+              id="longitude" 
+              required 
+              value={longitude}
+              onChange={e => setLongitude(e.target.value)}
+              />
+            </div>
+
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+      <ul>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/11440712?s=460&v=4" alt="Marcelo Gouveia"/>
+            <div className="user-info">
+              <strong>Marcelo Gouveia</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+          </header>
+          <p>Texto para teste só pra testar mesmo o container porque já são 4 da manhã e eu queria mesmo era estar dormindo na minhca cama quentinha</p>
+          <a href="https://github.com/MarceloGouveia77">GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/11440712?s=460&v=4" alt="Marcelo Gouveia"/>
+            <div className="user-info">
+              <strong>Marcelo Gouveia</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+          </header>
+          <p>Texto para teste só pra testar mesmo o container porque já são 4 da manhã e eu queria mesmo era estar dormindo na minhca cama quentinha</p>
+          <a href="https://github.com/MarceloGouveia77">GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/11440712?s=460&v=4" alt="Marcelo Gouveia"/>
+            <div className="user-info">
+              <strong>Marcelo Gouveia</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+          </header>
+          <p>Texto para teste só pra testar mesmo o container porque já são 4 da manhã e eu queria mesmo era estar dormindo na minhca cama quentinha</p>
+          <a href="https://github.com/MarceloGouveia77">GitHub</a>
+        </li>
+
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/11440712?s=460&v=4" alt="Marcelo Gouveia"/>
+            <div className="user-info">
+              <strong>Marcelo Gouveia</strong>
+              <span>ReactJS, React Native, NodeJS</span>
+            </div>
+          </header>
+          <p>Texto para teste só pra testar mesmo o container porque já são 4 da manhã e eu queria mesmo era estar dormindo na minhca cama quentinha</p>
+          <a href="https://github.com/MarceloGouveia77">GitHub</a>
+        </li>
+      </ul>
+      </main>
     </div>
   );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// COMPONENTE: Bloco isolado de HTML, CSS, JS, o qual não interfere no restante da aplicação
+// ESTADO: Informações mantidas pelo componente (Lembrar: imutabilidade)
+// PROPRIEDADE: Informações que um componente PAI passa para o componente FILHO
